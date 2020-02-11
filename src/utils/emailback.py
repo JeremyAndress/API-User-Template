39957f9196db7ad180f1c2e8f96330c206a24583
JEREMYANDRESS <jsilva@qin.cl>
2020-02-11 12:02:10 -0300

    ADD : subject

--- src/utils/emailback.py
+++ src/utils/emailback.py
@@ -2,11 +2,10 @@ from django.core.mail import EmailMessage
 from django.template import Context
 from django.template.loader import get_template
 
-
-
 def send_email():
-    template = get_template('email/email.html')
-    context = Context({'user': user, 'other_info': info})
+    subject, from_email, to = 'Subject', 'from@xxx.com', 'jeremysilvasilva@gmail.com'
+    template = get_template('email/activate.html')
+    context = Context({'header':header,'body':body})
     content = template.render(context)
-    msg = EmailMessage(subject, content, from, to=[user.email,])
+    msg = EmailMessage(subject, content, from_email,[to])
     msg.send()

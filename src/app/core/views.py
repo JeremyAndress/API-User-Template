7ae5beb8e1b782a3514a7f3b211bc6a8d0b8e6b6
JEREMYANDRESS <jsilva@qin.cl>
2020-02-12 15:32:31 -0300

    ADD : bootstrap email

--- src/app/core/views.py
+++ src/app/core/views.py
@@ -2,10 +2,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from django.shortcuts import render
+from utils.emailback import send_email
 
 def index(request):
+    #send_email()
     return render(request,'email/activate.html')
-
 
 @api_view(['GET'])
 def api_root(request, format=None):
--- src/utils/emailback.py
+++ src/utils/emailback.py
@@ -1,11 +1,32 @@
 from django.core.mail import EmailMessage
+from django.core.mail import EmailMultiAlternatives
 from django.template import Context
 from django.template.loader import get_template
+from django.template.loader import render_to_string
 
 def send_email():
-    subject, from_email, to = 'Subject', 'from@xxx.com', 'jeremysilvasilva@gmail.com'
-    template = get_template('email/activate.html')
-    context = Context({'header':header,'body':body})
-    content = template.render(context)
-    msg = EmailMessage(subject, content, from_email,[to])
+    subject, from_email = 'Prueba css', 'misperrisduoc.duoc@gmail.com'
+    to = ['jrojas@qin.cl', 'jeremysilvasilva@gmail.com']
+    preview = {'msj':'Localhost : message'}
+    header = {'msj': 'Gracias Por Registrarte','msj2':'Para activar su cuenta acceda al siguiente link'}
+    lorem = '''Lorem ipsum dolor sit amet, 
+    consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+    laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+    irure dolor in reprehenderit in voluptate velit esse cillum 
+    dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+    cupidatat non proident, sunt in culpa qui officia deserunt 
+    mollit anim id est laborum.'''
+    body =  {'msj':lorem}
+    ctx = {'preview':preview,'header':header,'body':body}
+    template = get_template('email/activate-email.html').render(ctx)
+    print(template)
+    msg = EmailMultiAlternatives(subject, template, from_email, to)
+    msg.attach_alternative(template, "text/html")
     msg.send()
+
+
+
+
+

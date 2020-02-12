16af1b56928d935623153551ab6422deaf9c45ed
JEREMYANDRESS <jsilva@qin.cl>
2020-02-12 15:56:10 -0300

    REMOVE : delete email def

--- src/app/core/views.py
+++ src/app/core/views.py
@@ -2,10 +2,12 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from django.shortcuts import render
-from utils.emailback import send_email
 
-def index(request):
-    #send_email()
+def email_send(request):
+    from utils.emailback import send_email
+    '''Bootstrapemail parsea html para ser renderizado por email'''
+    #https://bootstrapemail.com
+    send_email()
     return render(request,'email/activate.html')
 
 @api_view(['GET'])
